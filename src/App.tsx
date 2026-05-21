3836b46649a07b0ae0db3f36305eccfa941e16a5
GagaPoloJr <rahmadiihaikal@gmail.com>
2026-05-22 01:22:58 +0700

    feat: implement theme-based styling and update home page UI components

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   // keep it this as a global wrapper for all the page
   return (
     <>
-      <main className="h-screen w-screen bg-slate-900">
+      <main className="bg-background h-screen w-screen">
         <div className="h-full w-full px-4 py-6 md:px-32 md:py-16">
           <Outlet />
         </div>
--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -6,16 +6,16 @@ export default function HomePage() {
 
   return (
     <div className="p-5">
-      <h1 className="mb-5 text-2xl font-bold text-slate-100">GSAP Lab</h1>
+      <h1 className="text-heading mb-5 text-2xl font-bold">GSAP Lab</h1>
       <div className="flex flex-col space-y-2">
         {laboratoryExperiments.map((lab) => (
           <div
             key={lab.slug}
             onClick={() => navigate(`/laboratory/${lab.slug}`)}
-            className="cursor-pointer rounded-lg border border-slate-700 bg-slate-800 p-3 text-slate-200 hover:border-slate-600"
+            className="border-blak/90 cursor-pointer rounded-lg border bg-white p-3 hover:border-slate-600"
           >
-            <h2 className="font-medium">{lab.title}</h2>
-            <p className="text-sm text-slate-400">{lab.description}</p>
+            <h2 className="text-heading font-medium">{lab.title}</h2>
+            <p className="text-black/60">{lab.description}</p>
           </div>
         ))}
       </div>
